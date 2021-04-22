4bb5e1b6ed1fc98fe7c8b17d89560f17b05f1dd3
Vinicius Farias <fsvinicius9@gmail.com>
2021-04-22 15:57:20 -0300

    player json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,15 @@
+// SPA
+// SSR
+// SSG
+
+import { useEffect } from "react"
+
 export default function Home() {
+  useEffect(()=> {
+    fetch('http://localhost:3333/episodes')
+    .then(response => response.json)
+    .then(data => console.log(data))
+  }, [])
   return (
     <h1>Index</h1>
   )
